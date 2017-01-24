ae04a9c641327bb2cf7ba8fea153362f393e74da
Kadyrov96 <kadyrov96@mail.ru>
2017-01-24 19:47:42 +0300

    Added interface and its implementation of class, that handle receiving and sending files by server.

---
+++ IOStreamHandler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Net.Security;
+using System.Text;
+
+namespace TEST2
+{
+    class IOStreamHandler: IStreamHandler
+    {
+        private SslStream SSL_Stream;
+        private static int bufSize = 2048;
+        private string fileName;
+        private long fileSize;
+        FileStream downWriter;
+
+        private byte[] receiveBuffer = new byte[bufSize];
+        private byte[] sendBuffer;
+        private byte[] bytedFileName;
+        private byte[] bytedFileSize;
+        void IStreamHandler.ClearBuffers()
+        {
+            downWriter.Close();
+            downWriter.Dispose();
+        }
+
+        void IStreamHandler.ReceiveData(string savingFolderPath)
+        {
+            if (SSL_Stream.CanRead)
+            {
+                //Reading first field of incoming packet - name of the file
+                int bytesSize = SSL_Stream.Read(receiveBuffer, 0, bufSize);
+                fileName = Encoding.Unicode.GetString(receiveBuffer, 0, bytesSize);
+
+                //Reading second field of incoming packet - size of the file
+                receiveBuffer = new byte[bufSize];
+                bytesSize = SSL_Stream.Read(receiveBuffer, 0, bufSize);
+                fileSize = Convert.ToInt64(Encoding.Unicode.GetString(receiveBuffer, 0, receiveBuffer.Length));
+
+                //Reading file content
+                receiveBuffer = new byte[fileSize];
+                downWriter = File.Create(savingFolderPath + fileName);
+                bytesSize = SSL_Stream.Read(receiveBuffer, 0, receiveBuffer.Length);
+                downWriter.Write(receiveBuffer, 0, bytesSize);
+            }
+        }
+
+        void IStreamHandler.SendData(string filePath)
+        {
+            if (SSL_Stream.CanWrite)
+            {
+                //Getting info about selected file
+                FileInfo fInfo = new FileInfo(filePath);
+                fileName = fInfo.Name;
+                fileSize = fInfo.Length;
+
+                //Sending first field of the packet - name of the file
+                bytedFileName = new byte[bufSize];
+                bytedFileName = Encoding.Unicode.GetBytes(fileName.ToCharArray());
+                SSL_Stream.Write(bytedFileName, 0, bufSize);
+
+                //Sending second field of the packet - size of the file
+                bytedFileSize = new byte[bufSize];
+                bytedFileSize = Encoding.Unicode.GetBytes(fileSize.ToString().ToCharArray());
+                SSL_Stream.Write(bytedFileSize, 0, bufSize);
+
+                //System.Windows.Forms.MessageBox.Show("Sending the file " + fileName + " (" + fileSize + " bytes)\r\n");
+                //Sending file content
+                sendBuffer = new byte[fileSize];
+                sendBuffer = File.ReadAllBytes(filePath);
+                SSL_Stream.Write(sendBuffer, 0, sendBuffer.Length);
+            }
+        }
+    }
+}
---
+++ IStreamHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST2
+{
+    internal interface IStreamHandler
+    {
+        void ReceiveData(string savingFolderPath);
+        void SendData(string filePath);
+        void ClearBuffers();
+    }
+}
