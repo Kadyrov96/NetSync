6daf5b8ccce73eb25aff9b56654844bc1c0b795a
Kadyrov96 <kadyrov96@mail.ru>
2017-01-29 00:06:06 +0300

    Added new class, that handles with names of files and keys (depends on type of file modification: deleted, added, modified).

---
+++ HashRecord.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace TEST2
+{
+    class SyncRecordList
+    {
+        private int count;
+        public int Count
+        {
+            private set
+            {
+                count = namesList.Count;
+            }
+            get
+            {
+                return count;
+            }
+        }
+        public List<string> names;
+        public List<int> keys;
+        public SyncRecordList()
+        {
+            names = new List<string>();
+            keys = new List<int>();
+        }
+
+        public void AddRecord(string name, int key)
+        {
+            names.Add(name);
+            keys.Add(key);
+        }
+
+        public void RemoveAt(int index)
+        {
+            names.RemoveAt(index);
+            keys.RemoveAt(index);
+        }
+
+    }
+}
