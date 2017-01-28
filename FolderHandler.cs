1ae779bffeb1cfa4bbcc9d30e696dd87551b1218
Kadyrov96 <kadyrov96@mail.ru>
2017-01-28 12:41:34 +0300

    Added a class, that operates with filesystem folders and their entries.

---
+++ FolderHandler.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using System.Windows.Forms;
+
+namespace TEST2
+{
+    class FolderHandler
+    {
+        private string folderPath;
+        public string FolderPath
+        {
+            private set
+            {
+                folderPath = value;
+            }
+            get
+            {
+                return folderPath;
+            }
+        }
+
+        private string[] folderElements;
+        public string[] FolderElements
+        {
+            private set
+            {
+                folderElements = value;
+            }
+            get
+            {
+                return folderElements;
+            }
+        }
+
+        public void SelectFolder()
+        {
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                FolderPath = folderBrowserDialog.SelectedPath;
+                FolderElements = Directory.GetFileSystemEntries(folderPath);
+            }
+        }
+    }
+}
